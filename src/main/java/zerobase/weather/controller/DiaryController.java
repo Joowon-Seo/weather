de9f0732a285ed96f91804db6f35cec67855b0fe
Juwon <tjwndnjs1998@gmail.com>
2022-09-06 22:52:14 +0900

    feat : openweatherAPI url 연동
    openweatherApi url의 key가 유효한지 확인

--- src/main/java/zerobase/weather/controller/DiaryController.java
+++ src/main/java/zerobase/weather/controller/DiaryController.java
@@ -3,13 +3,13 @@ package zerobase.weather.controller;
 import java.time.LocalDate;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import zerobase.weather.service.DiaryService;
 
-@Controller
+@RestController
 public class DiaryController {
 	private final DiaryService diaryService;
 
