9230463c7b9d7308bad9a7692d7c3dfe642104f2
Juwon <tjwndnjs1998@gmail.com>
2022-09-06 17:26:03 +0900

    feat : createDiary메소드 구현
    클라이언트에서 오는 Post요청을 받는 createDiary Controller 구현

--- src/main/java/zerobase/weather/controller/DiaryController.java
+++ src/main/java/zerobase/weather/controller/DiaryController.java
@@ -1,8 +1,25 @@
 package zerobase.weather.controller;
 
+import java.time.LocalDate;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import zerobase.weather.service.DiaryService;
 
 @Controller
 public class DiaryController {
+	private final DiaryService diaryService;
 
+	public DiaryController(DiaryService diaryService) {
+		this.diaryService = diaryService;
+	}
+
+	@PostMapping("/create/diary")
+	void createDiary(@RequestParam @DateTimeFormat(iso = ISO.DATE) LocalDate date,
+		@RequestBody String text) {
+		diaryService.createDiary(date, text);
+	}
 }
--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -1,8 +1,12 @@
 package zerobase.weather.service;
 
+import java.time.LocalDate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DiaryService {
 
+	public void createDiary(LocalDate date, String text) {
+
+	}
 }
