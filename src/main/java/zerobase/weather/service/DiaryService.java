f49b59eefa83bc4aa060f3bdd3f7aec86233ed47
Juwon <tjwndnjs1998@gmail.com>
2022-09-06 23:19:06 +0900

    feat : open weather map에서 날씨 데이터 가져오기
    이건 commit에서 유효한 apikey를 가지고 open weather map에서 날씨
    데이터를 가져오기

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -1,6 +1,10 @@
 package zerobase.weather.service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.time.LocalDate;
+import javax.net.ssl.HttpsURLConnection;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +15,32 @@ public class DiaryService {
 	private String apiKey;
 
 	public void createDiary(LocalDate date, String text) {
-		getWeatherString();
+		String weatherData = getWeatherString();
 	}
 
-	private String getWeatherString(){
+	private String getWeatherString() {
 		String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=" + apiKey;
-		System.out.println(apiUrl);
-		return "";
+
+		try {
+			URL url = new URL(apiUrl);
+			HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			int responseCode = connection.getResponseCode();
+			BufferedReader br;
+			if (responseCode == 200) {
+				br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			} else {
+				br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+			}
+			String inputLine;
+			StringBuilder response = new StringBuilder();
+			while ((inputLine = br.readLine()) != null) {
+				response.append(inputLine);
+			}
+			br.close();
+			return response.toString();
+		} catch (Exception e) {
+			return "failed to get response";
+		}
 	}
 }
